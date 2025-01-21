391d5f8c4effef1da6323ef2b245ae003d679a8b
monsieurjune <june.monsieurjune@gmail.com>
2025-01-21 17:09:39 +0700

    (reset): Delete ft_irc related code from include/ & src/
    Delete them for good placing restart and applied code
    That defined in UML Class Diagram easier

--- src/irc_cmd/ircCommandKICK.cpp
+++
@@ -1,8 +0,0 @@
-#include "ft_irc/FtIrcMemory.hpp"
-
-// TODO: Define logic in this
-
-std::string	FtIrcMemory::ircCommandKICK(Message const & message, Client const * const sender)
-{
-	return "";
-}
--- src/irc_cmd/ircCommandMODE.cpp
+++
@@ -1,8 +0,0 @@
-#include "ft_irc/FtIrcMemory.hpp"
-
-// TODO: Define logic in this
-
-std::string	FtIrcMemory::ircCommandMODE(Message const & message, Client const * const sender)
-{
-	return "";
-}
