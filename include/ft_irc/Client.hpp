f4533e0813497f5f2ffa6283f75a6423de087374
monsieurjune <june.monsieurjune@gmail.com>
2025-01-22 02:53:58 +0700

    (feat): Migrate Client's Methods with additional modification
    Migrate All Client's Method with modification that define in UML
    Add their assosiate doxygen document & fix minor inconvienent in doxygen

--- include/ft_irc/Client.hpp
+++ include/ft_irc/Client.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnualman <tnualman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 17:31:35 by tponutha          #+#    #+#             */
-/*   Updated: 2025/01/21 22:28:22 by tnualman         ###   ########.fr       */
+/*   Updated: 2025/01/22 02:53:11 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,10 @@ class Client
 	private:
 		/**
 		 * @brief The socket's fd of Client
+		 * 
+		 * @note Only close it in deconstructor's scope
 		 */
-		int	_fd;
+		int const	_fd;
 		
 		/**
 		 * @brief Time when client is connected
@@ -50,13 +52,20 @@ class Client
 		 * @brief Authenicate Level of IRC Client
 		 * 
 		 * Value that store authenicate levelaccording to IRC Protocol
-		 * This Value use bitwise flag to define behaviour
-		 * Multiple flags can be combined using the bitwise OR (`|`) operator. 
+		 * and use bitwise flag to define behaviour
+		 * 
+		 * Multiple flags can be combined using the bitwise OR (`|`) operator.
+		 * 
 		 * Possible values are:
+		 * 
 		 * - 0x01 (PASS FLAG): Verify Password Authenication
+		 * 
 		 * - 0x02 (USER FLAG): Verify Username Authenication
+		 * 
 		 * - 0x04 (NICK FLAG): Verify Nickname Authenication
+		 * 
 		 * - 0x08 (LOGIN FALG): Verify that this Client is passed all authenication
+		 * 
 		 * - 0x16 (DEBUG FLAGE): Use for ignore authenication for debugging purpose
 		 * 
 		 * @note This Attribute can be ignored if DEBUG flag is setted
@@ -73,46 +82,48 @@ class Client
 		/**
 		 * @brief Queue for RESPONSE messages
 		 * 
-		 * This Queue used for storing these RESOPONSE messages
-		 * Before Sending out in POLLOUT part, one per pollfd loop in FIFO order
+		 * This Queue used for storing these RESOPONSE messages,
+		 * before Sending out in POLLOUT part, one per pollfd loop in FIFO order
 		 * 
-		 * @note Don't send anything, if this queue is empty
+		 * @warning 
+		 * - Should dequeue out and send one time per poll loop
+		 * 
+		 * - Don't send anything, if this queue is empty
 		 */
 		std::queue<std::string>	_replyQueue;
 
 		/**
 		 * @brief Set of Channel that this Client is membershiped
 		 * 
-		 * This Set is used for storing Channel Pointer
-		 * That this Client is membershiped of
-		 * Use for ease of "cleaning" when Client is disconnected
+		 * This Set is used for storing Channel Pointer, 
+		 * which this Client is membershiped of.
 		 * 
-		 * @note This attribute migth be use for anything else, but write the comment
+		 * @warning Must unmembership, when deconstructor call
 		 */
 		std::set<Channel*>	_channel;
 
 		/**
 		 * @brief Client's Username
 		 * 
-		 * It can set through USER command
+		 * @note It can set through USER command
 		 * 
-		 * @note It can't change, once set
+		 * @warning It mustn't be changed, once set.
 		 */
 		std::string	_username;
 
 		/**
 		 * @brief Client's Nickname
 		 * 
-		 * It can set/change through NICK command
+		 * @note It can set/change through NICK command
 		 */
 		std::string	_nickname;
 
 		/**
 		 * @brief Client's realname
 		 * 
-		 * It can set through USER command
+		 * @note It can set through USER command
 		 * 
-		 * @note It can't change, once set
+		 * @warning It mustn't be changed, once set.
 		 */
 		std::string	_realname;
 
@@ -122,6 +133,207 @@ class Client
 		 * @note It should be set after client is connected
 		 */
 		std::string	_host;
+
+	public:
+		/**
+		 * @brief Constructs an Client object
+		 * 
+		 * @param fd This Object's socket fd
+		 */
+		Client(int const fd);
+		
+		/**
+		 * @brief Default Destructor
+		 */
+		~Client();
+
+		// GETTER
+
+		/**
+		 * @brief Get This Client's socket fd
+		 * 
+		 * @return This Client's socket fd
+		 * 
+		 * @note Do not attempt to close this fd outside of Destructor's scope
+		 */
+		int	getFd()	const;
+
+		/**
+		 * @brief Get This Client's time when connected
+		 * 
+		 * @return Time when This Client is constructed
+		 */
+		time_t	getTimeConnected()	const;
+
+		/**
+		 * @brief Get This Client's Authenication Level
+		 * 
+		 * @return Integer that Indicate authenication level
+		 * 
+		 * @note Use bitwise to extract authenicate flag
+		 */
+		int	getAuthenLevel()	const;
+
+		/**
+		 * @brief Get This Client's Nickname
+		 * 
+		 * @return This Client's Nickname as rvalue
+		 */
+		std::string const&	getNickname()	const;
+
+		/**
+		 * @brief Get This Client's Username
+		 * 
+		 * @return This Client's Username as rvalue
+		 */
+		std::string const&	getUsername()	const;
+
+		/**
+		 * @brief Get This Client's Realname
+		 * 
+		 * @return This Client's Realname as rvalue
+		 */
+		std::string const&	getRealname()	const;
+
+		/**
+		 * @brief Get This Client's Host Information
+		 * 
+		 * @return This Client's Host Information as rvalue
+		 */
+		std::string const&	getHost()	const;
+
+		/**
+		 * @brief Get Set of Channels that This Client is membered
+		 * 
+		 * @return Set of Channels Pointer as rvalue
+		 */
+		std::set<Channel*> const&	getChannels()	const;
+
+		/**
+		 * @brief Get This Client's Membership Mode
+		 * 
+		 * @return Set of Charactors that represent membership mode
+		 */
+		std::set<char> const&	getModes()	const;
+
+		/**
+		 * @brief Check if This Client contain certain mode
+		 * 
+		 * @return True if contain this mode, false otherwise
+		 */
+		bool	hasMode(char c)	const;
+
+		/**
+		 * @brief Check if This Client is membershiped of certain Channel
+		 * 
+		 * @return True if contain this Channel, false otherwise
+		 */
+		bool	isInChannel(Channel * const channel)	const;
+
+		/**
+		 * @brief Set This Client's authenication level
+		 * 
+		 * @param level Authenication Level
+		 * 
+		 * @note This Method is totally remove old one. If you want to 'append' new flag.
+		 * You can get old value and 'bitwise OR' to new value
+		 */
+		void	setAuthenLevel(int const level);
+
+		/**
+		 * @brief Set This Client's Nickname
+		 * 
+		 * @param name Nickname
+		 * 
+		 * @warning It must be called in NICK's fucntion
+		 */
+		void	setNickname(std::string const name);
+
+		/**
+		 * @brief Set This Client's Username
+		 * 
+		 * @param name Username
+		 * 
+		 * @warning It must be called in USER's function
+		 */
+		void	setUsername(std::string const name);
+
+		/**
+		 * @brief Set This Client's Realname
+		 * 
+		 * @param name Realname
+		 * 
+		 * @warning It must be called in USER's function
+		 */
+		void	setRealname(std::string const name);
+
+		/**
+		 * @brief Set This Client's Host Information
+		 * 
+		 * @param host Host Information
+		 * 
+		 * @warning It must be called after Client is built
+		 */
+		void	setHost(std::string const host);
+
+		/**
+		 * @brief Add This Membership Mode to This Client
+		 * 
+		 * @param c Membership Character
+		 */
+		void	addMode(char const c);
+
+		/**
+		 * @brief Remove This Membership Mode from This Client
+		 * 
+		 * @param c Membership Charactor
+		 */
+		void	removeMode(char const c);
+
+		/**
+		 * @brief Add This Channel to Set of Channel of This Client
+		 * 
+		 * @param channel Channel that This Client is recently joined
+		 * 
+		 * @warning This Client must be under This Channel's Membership already
+		 * 
+		 * @note Call it under Channel's add-client method
+		 */
+		void	addToChannel(Channel * const channel);
+
+		/**
+		 * @brief Remove This Channel to Set of Channel of This Client
+		 * 
+		 * @param channel Channel that This Client want to remove
+		 * 
+		 * @warning This Client must be under This Channel's Membership already
+		 * 
+		 * @note Call it under Channel's remove-client method
+		 */
+		void	removeFromChannel(Channel * const channel);
+
+		/**
+		 * @brief Get Amount of Message in Queue
+		 * 
+		 * @return Amount of Message
+		 */
+		size_t	countReply()	const;
+
+		/**
+		 * @brief Enqueue This Message
+		 * 
+		 * @param msg IRC Response Message
+		 */
+		void	enqueueReply(std::string const msg);
+
+		/**
+		 * @brief Dequeue This Message
+		 * 
+		 * @return IRC Response Message in lvalue
+		 * 
+		 * @warning Only Use it in function that send msg out
+		 */
+		std::string	dequeueReply();
 };
 
 #endif
