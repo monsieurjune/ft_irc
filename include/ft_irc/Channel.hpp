1a3cad504a9646174b5d554234d7f71e0aeae715
monsieurjune <june.monsieurjune@gmail.com>
2025-01-22 05:53:44 +0700

    (feat): Add Constructor/Destructor, Getter in Channel
    Add Constructor/Destructor & Getter in Channel Class
    Add _userCountLimit attribute in Channel
    
    Note:
    I edited Constructor differ from what we planned
    As Channel should has its creator in the first place

--- include/ft_irc/Channel.hpp
+++ include/ft_irc/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 02:56:42 by tponutha          #+#    #+#             */
-/*   Updated: 2025/01/22 03:56:57 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/01/22 05:53:31 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,11 @@ class Channel
 		std::map<Client *, std::set<char> >	_userMap;
 
 		/**
+		 * @brief Maximum User in This Channel. Ignore if userlimit mode is unset
+		 */
+		size_t	_userCountLimit;
+
+		/**
 		 * @brief Channel's Modes
 		 */
 		std::set<char>	_modes;
@@ -74,6 +79,82 @@ class Channel
 		 * @brief The Nickname of most recent user that set topic
 		 */
 		std::string	_topicSetter;
+
+	public:
+		/**
+		 * @brief Construct an Channel object
+		 * 
+		 * @param name Channel's Name
+		 * @param creator Client that requested to create channel
+		 */
+		Channel(std::string const name, Client * const creator);
+		
+		/**
+		 * @brief Default Destructor
+		 * 
+		 * @note 
+		 * Should pop out this channel from client's channel set.
+		 * Despite the fact that, it must have 0 user to be deleted
+		 */
+		~Channel();
+
+		/**
+		 * @brief Get Channel's Name
+		 * 
+		 * @return Channel's Name in rvalue
+		 */
+		std::string const&	getName()	const;
+
+		/**
+		 * @brief Get Time when this channel is created
+		 * 
+		 * @return Time when this channel is created
+		 */
+		time_t	getTimeCreated()	const;
+
+		/**
+		 * @brief Get Amount of Client that joined this Channel
+		 * 
+		 * @return Amount of Client that stored in map
+		 */
+		size_t	getUserCount()	const;
+
+		/**
+		 * @brief Get Amount of User Limit
+		 * 
+		 * @return If userlimit flag is setted, return N, otherwise return 0
+		 */
+		size_t	getUserCountLimit()	const;
+
+		/**
+		 * @brief Get This Channel Topic's Name
+		 * 
+		 * @return Topic Name in rvalue
+		 */
+		std::string const&	getTopic()	const;
+
+		/**
+		 * @brief Get Most Recent Time that topic is setted
+		 * 
+		 * @return Most Recent Time that topic is setted
+		 */
+		time_t	getTimeTopicSet()	const;
+
+		/**
+		 * @brief Get Most Recent Topic Setter's Nickname
+		 * 
+		 * @return Nickname of Most Recent Topic Setter in rvalue
+		 */
+		std::string const&	getTopicSetter()	const;
+
+		/**
+		 * @brief Get Channel Modes 
+		 * 
+		 * @note not to be confused with client's membership modes
+		 * 
+		 * @return Channel Mode Set in rvalue
+		 */
+		std::set<char> const&	getModes()	const;
 };
 
 #endif
