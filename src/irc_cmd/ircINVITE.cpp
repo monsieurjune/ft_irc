818eec6969908a3762a79fc252bbd91377e4776e
Sirapat Charukamnertkanok <sirapat1charu@gmail.com>
2025-01-31 19:22:07 +0700

    adding function to the file

--- src/irc_cmd/ircINVITE.cpp
+++ src/irc_cmd/ircINVITE.cpp
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 18:33:17 by scharuka          #+#    #+#             */
-/*   Updated: 2025/01/31 18:35:50 by scharuka         ###   ########.fr       */
+/*   Updated: 2025/01/31 19:12:35 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,8 @@
 #include "ft_irc/Client.hpp"
 #include "ft_irc/Channel.hpp"
 #include "ft_irc/Message.hpp"
+
+FtIrc::t_replyBatch FtIrc::ircINVITE(FtIrc * const obj, Message const & msg, Client * const client)
+{
+	
+}
--- src/irc_cmd/ircPRIVMSG.cpp
+++ src/irc_cmd/ircPRIVMSG.cpp
@@ -6,7 +6,7 @@
 /*   By: scharuka <scharuka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 18:36:40 by scharuka          #+#    #+#             */
-/*   Updated: 2025/01/31 18:37:00 by scharuka         ###   ########.fr       */
+/*   Updated: 2025/01/31 19:12:54 by scharuka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,7 @@
 #include "ft_irc/Channel.hpp"
 #include "ft_irc/Message.hpp"
 
+FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Client * const client)
+{
+		
+}
