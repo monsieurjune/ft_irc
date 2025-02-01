d83f5a010ab2f0fbd51bcf6fa6c7a36da68f3210
monsieurjune <june.monsieurjune@gmail.com>
2025-02-01 18:37:44 +0700

    (feat): Finish PING, PONG, CAP
    Note:
    - this server wouldn't like to implement capabilities, so ignore CAP
    - trailing character is missing when create PONG reply

--- src/irc_cmd/ircPING.cpp
+++ src/irc_cmd/ircPING.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 18:33:17 by scharuka          #+#    #+#             */
-/*   Updated: 2025/02/01 14:04:11 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/02/01 18:34:05 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,33 @@
 #include "ft_irc/Client.hpp"
 #include "ft_irc/Channel.hpp"
 #include "ft_irc/Message.hpp"
+#include "ft_irc/ircReply.hpp"
 
 FtIrc::t_replyBatch	FtIrc::ircPING(FtIrc * const obj, Message const & msg, Client * const client)
 {
-	
+	std::vector<std::string> const&	params	= msg.getParams();
+
+	// Invalid size of Params
+	if (params.size() != 1)
+	{
+		return errNeedMoreParams(obj, client, msg.getCommand());
+	}
+
+	Message	reply_msg;
+
+	// Creating MSG
+	reply_msg.setSource(obj->getServerName());
+	reply_msg.setCommand("PONG");
+	reply_msg.pushParam(obj->getServerName());
+	reply_msg.pushParam(params.at(0));
+
+	// Put it in proper struct
+	std::queue<Message>	queue;
+
+	queue.push(reply_msg);
+
+	FtIrc::t_reply		reply(client, queue);
+	FtIrc::t_replyBatch	batch(1, reply);
+
+	return batch;
 }
