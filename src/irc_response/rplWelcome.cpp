58e996c9f38db444554b8c6a995b5f3bc9f2d15d
monsieurjune <june.monsieurjune@gmail.com>
2025-04-19 00:53:35 +0700

    (feat): Define code structure of rplWelcome()
    Feat:
    - Define overall process of rplWelcome()
    - Add barebone code of rpl_WELCOME
    - Add barebone code of rpl_YOURHOST
    - Add barebone code of rpl_CREATED
    - Add barebone code of rpl_MYINFO
    - Add barebone code of rpl_ISUPPORT

--- src/irc_response/rplWelcome.cpp
+++ src/irc_response/rplWelcome.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/13 09:33:35 by tponutha          #+#    #+#             */
-/*   Updated: 2025/04/13 21:41:02 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/04/19 00:49:39 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,56 @@
 #include "ft_irc/Client.hpp"
 #include "ft_irc/Channel.hpp"
 #include "ft_irc/Message.hpp"
-#include "std/ft_cppstd.hpp"
+#include "ft_irc/FtIrcFunctionHelper.hpp"
+
+static Message  sb_rpl_WELCOME(FtIrc * const obj, Client * const client)
+{
+    
+}
+
+static Message  sb_rpl_YOURHOST(FtIrc * const obj, Client * const client)
+{
+    
+}
+
+static Message  sb_rpl_CREATED(FtIrc * const obj, Client * const client)
+{
+    
+}
+
+static Message  sb_rpl_MYINFO(FtIrc * const obj, Client * const client)
+{
+
+}
+
+static FtIrc::t_reply   sb_rpl_ISUPPORT(FtIrc * const obj, Client * const client)
+{
+
+}
 
 FtIrc::t_replyBatch FtIrc::rplWelcome(Client * const client)
 {
-    (void)client;
+    FtIrc::t_reply  welcome_reply;
+    FtIrc::t_reply  issuport_reply;
 
-    return t_replyBatch();
+    // 001, 002, 003, 004
+    welcome_reply.first = client;
+    welcome_reply.second.push(sb_rpl_WELCOME(this, client));
+    welcome_reply.second.push(sb_rpl_YOURHOST(this, client));
+    welcome_reply.second.push(sb_rpl_CREATED(this, client));
+    welcome_reply.second.push(sb_rpl_MYINFO(this, client));
+
+    // 005
+    issuport_reply = sb_rpl_ISUPPORT(this, client);
+
+    // 375, 372, 376
+    // TODO: do later
+
+    // Append to Reply Batch
+    FtIrc::t_replyBatch batch(2);
+
+    batch.push_back(welcome_reply);
+    batch.push_back(issuport_reply);
+
+    return batch;
 }
