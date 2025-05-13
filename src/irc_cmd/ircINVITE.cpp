fa018756c86308408373a909a2794bc0bddf2862
Tattananan Nualmanee <tnualman@z3t9c1.42bangkok.com>
2025-05-13 22:52:30 +0700

    Finished ircINVITE;

--- src/irc_cmd/ircINVITE.cpp
+++ src/irc_cmd/ircINVITE.cpp
@@ -6,7 +6,7 @@
 /*   By: tnualman <tnualman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 18:33:17 by scharuka          #+#    #+#             */
-/*   Updated: 2025/05/11 19:02:25 by tnualman         ###   ########.fr       */
+/*   Updated: 2025/05/13 22:51:46 by tnualman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,16 @@ FtIrc::t_replyBatch FtIrc::ircINVITE(FtIrc * const obj, Message const & message,
 	Message						reply_msg;
 	t_reply						reply_sender;
 	t_replyBatch				batch;
-	std::string					nick = sender->getNickname();
+	std::string					sender_nick = sender->getNickname();
 
 	reply_sender.first = sender;
+	reply_msg.setSource(obj->_serverName);
 
-    if (params.size() == 0)
+    // INVITE_LIST
+	if (params.size() == 0)
     {
-		reply_msg.setSource(obj->_serverName);
 		reply_msg.setCommand(RPL_INVITELIST);
-		// Iterate through list of channels in server
+		// Iterate through list of channels in serve             r
 		std::map<std::string, Channel *>			tmpChannelMap = obj->_channelMapByName;
 		std::map<std::string, Channel *>::iterator	it = tmpChannelMap.begin();
 		for (it; it != tmpChannelMap.end(); it++)
@@ -39,41 +40,40 @@ FtIrc::t_replyBatch FtIrc::ircINVITE(FtIrc * const obj, Message const & message,
 			Channel * channel = it->second;
 			if (channel->isClientInvited(sender))
 			{
-				reply_msg.pushParam(nick);
+				reply_msg.pushParam(sender_nick);
 				reply_msg.pushParam(channel->getName());
 				reply_sender.second.push(reply_msg);
 				reply_msg.resetParams();
 			}
 		}
 		// RPL_ENDOFINVITELIST
-		reply_msg.setCommand(RPL_INVITELIST);
-		reply_msg.pushParam(nick);
+		reply_msg.setCommand(RPL_ENDOFINVITELIST);
+		reply_msg.pushParam(sender_nick);
 		reply_msg.pushParam("End of /INVITE list.");
 		reply_sender.second.push(reply_msg);
 		batch.push_back(reply_sender);
 		return (batch);
     }
-    
-    if (params.size() == 1 || params.at(0).empty() || params.at(1).empty())
+
+    // ERR_NEEDMOREPARAMS
+	if (params.size() < 2 || params.at(0).empty() || params.at(1).empty())
 	{
 		return (obj->errNeedMoreParams(sender, message));
 	}
 
-    std::string invitee_nickname = params.at(0);
-    Client *    invitee = obj->getClientByNickname(invitee_nickname);
+    std::string invitee_nick = params.at(0);
+    Client *    invitee = obj->getClientByNickname(invitee_nick);
     std::string channel_name = params.at(1);
     Channel *   channel = obj->getChannelByName(channel_name);
 
     // ERR_NOSUCHCHANNEL or which error response?
-    if (!channel)
+    if (channel_name.at(0) != '#' || !channel)
     {
-        reply_msg.setSource(obj->_serverName);
 		reply_msg.setCommand(ERR_NOSUCHCHANNEL);
-		reply_msg.pushParam(sender->getNickname());
-		reply_msg.pushParam(channel_name.empty() ? "*" : channel_name);
+		reply_msg.pushParam(sender_nick);
+		reply_msg.pushParam(channel_name);
 		reply_msg.pushParam("No such channel.");
 		reply_sender.second.push(reply_msg);
-		reply_msg.resetParams();
 		batch.push_back(reply_sender);
 		return (batch);
     }
@@ -81,69 +81,72 @@ FtIrc::t_replyBatch FtIrc::ircINVITE(FtIrc * const obj, Message const & message,
     // ERR_NOSUCHNICK (not described in the modern doc, but I think this is needed.)
     if (!invitee)
     {
-        // reply_msg.setSource(obj->_serverName);
-		// reply_msg.setCommand(ERR_NOSUCHCHANNEL);
-		// reply_msg.pushParam(sender->getNickname());
-		// reply_msg.pushParam(channel_name.empty() ? "*" : channel_name);
-		// reply_msg.pushParam("No such channel.");
-		// reply_sender.second.push(reply_msg);
-		// reply_msg.resetParams();
-		// batch.push_back(reply_sender);
-		// return (batch);
+		reply_msg.setCommand(ERR_NOSUCHNICK);
+		reply_msg.pushParam(sender_nick);
+		reply_msg.pushParam(invitee_nick);
+		reply_msg.pushParam("No such nickname.");
+		reply_sender.second.push(reply_msg);
+		batch.push_back(reply_sender);
+		return (batch);
     }
 
-	// // Primary for loop here.
-	// for (unsigned long idx = 0; idx < channel_name_vec.size(); idx++)
-	// {
-	// 	std::string	channel_name = channel_name_vec.at(idx);
-	// 	Channel *	channel = channel_name.empty() ? NULL : obj->getChannelByName(channel_name);
+	// ERR_NOTONCHANNEL
+	if (!(channel->hasThisClient(sender)))
+	{
+		reply_msg.setCommand(ERR_NOTONCHANNEL);
+		reply_msg.pushParam(sender_nick);
+		reply_msg.pushParam(channel_name);
+		reply_msg.pushParam("You're not on that channel.");
+		reply_sender.second.push(reply_msg);
+		batch.push_back(reply_sender);
+		return (batch);
+	}
 
-	// 	// ERR_NOSUCHCHANNEL has different meaning!
+	// ERR_CHANOPRIVSNEEDED
+	if (channel->hasThisMode(MODE_INVITEONLY) && !(channel->hasThisClientMembershipMode(sender, MODE_OPERATOR)))
+	{
+		reply_msg.setCommand(ERR_CHANOPRIVSNEEDED);
+		reply_msg.pushParam(sender_nick);
+		reply_msg.pushParam(channel_name);
+		reply_msg.pushParam("You need channel operator privilege to send an invite for an invite-only channel.");
+		reply_sender.second.push(reply_msg);
+		batch.push_back(reply_sender);
+		return (batch);
+	}
 
-	// 	// Validate channel_name first.
-	// 	if (channel_name.at(0) != '#' || !channel)
-	// 	{
-	// 		reply_msg.setSource(obj->_serverName);
-	// 		reply_msg.setCommand(ERR_NOSUCHCHANNEL);
-	// 		reply_msg.pushParam(sender->getNickname());
-	// 		reply_msg.pushParam(channel_name.empty() ? "*" : channel_name);
-	// 		reply_msg.pushParam("No such channel.");
-	// 		reply_sender.second.push(reply_msg);
-	// 		reply_msg.resetParams();
-	// 		batch.push_back(reply_sender);
-	// 		continue ;
-	// 	}
+	// ERR_USERONCHANNEL
+	if (channel->hasThisClient(invitee))
+	{
+		reply_msg.setCommand(ERR_USERONCHANNEL);
+		reply_msg.pushParam(sender_nick);
+		reply_msg.pushParam(invitee_nick);
+		reply_msg.pushParam(channel_name);
+		reply_msg.pushParam("Invitee is already on channel/.");
+		reply_sender.second.push(reply_msg);
+		batch.push_back(reply_sender);
+		return (batch);
+	}
 
-	// 	if (!(channel->hasThisClient(sender)))
-	// 	{
-	// 		reply_msg.setSource(obj->_serverName);
-	// 		reply_msg.setCommand(ERR_NOTONCHANNEL);
-	// 		reply_msg.pushParam(sender->getNickname());
-	// 		reply_msg.pushParam(channel_name.empty() ? "*" : channel_name);
-	// 		reply_msg.pushParam("You're not on that channel.");
-	// 		reply_sender.second.push(reply_msg);
-	// 		reply_msg.resetParams();
-	// 		batch.push_back(reply_sender);
-	// 		continue ;
-	// 	}
+	// SUCCESS_CASE
+	{
+		reply_msg.setCommand(RPL_INVITING);
+		reply_msg.pushParam(sender_nick);
+		reply_msg.pushParam(invitee_nick);
+		reply_msg.pushParam(channel_name);
+		reply_sender.second.push(reply_msg);
+		batch.push_back(reply_sender);
+		reply_msg.resetParams();
+		
+		t_reply	reply_invitee;
+		
+		reply_invitee.first = invitee;
+		reply_msg.setSource(sender->constructSource());
+		reply_msg.setCommand("INVITE");
+		reply_msg.pushParam(invitee_nick);
+		reply_msg.pushParam(channel_name);
+		batch.push_back(reply_invitee);
 
-    //     // General/valid PARTing case here.
-	// 	{
-	// 		channel->deleteUserFromChannel(sender);
-	// 		reply_msg.setSource(sender->constructSource());
-	// 		reply_msg.setCommand("PART");
-	// 		reply_msg.pushParam(channel_name);
-	// 		if (!reason.empty())
-    //         {
-    //             reply_msg.pushParam(reason);
-    //         }
-    //         reply_sender.second.push(reply_msg);
-	// 		batch.push_back(reply_sender);
-	// 		// PARTing announcement to other channel members here.	
-	// 		obj->pushChannelReplyOthers(reply_msg, channel, batch, sender);
-	// 		reply_msg.resetParams();
-	// 		continue ;
-	// 	}
-	// }
+		return (batch);
+	}
 	return (batch);
 }
