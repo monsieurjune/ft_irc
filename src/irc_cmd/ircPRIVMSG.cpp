177933754935273ee7941388bda8c3f1141bde44
monsieurjune <june.monsieurjune@gmail.com>
2025-05-14 18:55:46 +0700

    (fix): Remove check '&' channel prefix in ircPRIVMSG
    Since i only planned to check '#' for prefix to avoid extra work

--- src/irc_cmd/ircPRIVMSG.cpp
+++ src/irc_cmd/ircPRIVMSG.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ircPRIVMSG.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tnualman <tnualman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 18:36:40 by scharuka          #+#    #+#             */
-/*   Updated: 2025/05/14 13:30:28 by tnualman         ###   ########.fr       */
+/*   Updated: 2025/05/14 18:50:16 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,7 @@
 FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Client * const client)
 {
     t_replyBatch batch;
-
     const std::vector<std::string>& params = msg.getParams();
-    
 
     if (params.size() < 2)
     {
@@ -31,7 +29,6 @@ FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Cl
 
     const std::string& target = params[0];
     const std::string& message = params[1];
-    
 
     if (message.empty())
     {
@@ -39,14 +36,16 @@ FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Cl
     }
 
     Message reply;
+
     reply.setSource(client->constructSource());
     reply.setCommand("PRIVMSG");
     reply.pushParam(target);
     reply.pushParam(message);
 
-    if (target[0] == '#' || target[0] == '&')
+    if (target[0] == '#')
     {
         Channel* channel = obj->getChannelByName(target);
+
         if (!channel)
         {
             Message err;
@@ -55,11 +54,12 @@ FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Cl
             err.pushParam(client->getNickname());
             err.pushParam(target);
             err.pushParam("No such channel");
-            
+
             t_reply reply_item;
             reply_item.first = client;
             reply_item.second.push(err);
             batch.push_back(reply_item);
+
             return batch;
         }
 
@@ -71,19 +71,20 @@ FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Cl
             err.pushParam(client->getNickname());
             err.pushParam(target);
             err.pushParam("Cannot send to channel");
-            
+
             t_reply reply_item;
             reply_item.first = client;
             reply_item.second.push(err);
             batch.push_back(reply_item);
+
             return batch;
         }
-
         obj->pushChannelReplyAll(reply, channel, batch);
     }
     else
     {
         Client* target_client = obj->getClientByNickname(target);
+
         if (!target_client)
         {
             Message err;
@@ -92,11 +93,12 @@ FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Cl
             err.pushParam(client->getNickname());
             err.pushParam(target);
             err.pushParam("No such nick/channel");
-            
+
             t_reply reply_item;
             reply_item.first = client;
             reply_item.second.push(err);
             batch.push_back(reply_item);
+
             return batch;
         }
 
