1109c35b81d0e4e42075ef3a85bfa33d2221b628
Tattananan Nualmanee <tnualman@z2t2c4.42bangkok.com>
2025-05-18 10:33:35 +0700

    Added multiple targets handling for ircPRIVMSG;

--- src/irc_cmd/ircPRIVMSG.cpp
+++ src/irc_cmd/ircPRIVMSG.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ircPRIVMSG.cpp                                     :+:      :+:    :+:   */
+/*   ircPRIVMSG_new.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: scharuka <scharuka@42.fr>                  +#+  +:+       +#+        */
+/*   By: tnualman <tnualman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 18:36:40 by scharuka          #+#    #+#             */
-/*   Updated: 2025/05/18 09:21:28 by scharuka         ###   ########.fr       */
+/*   Updated: 2025/05/18 10:22:03 by tnualman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,95 +18,106 @@
 
 FtIrc::t_replyBatch FtIrc::ircPRIVMSG(FtIrc * const obj, Message const & msg, Client * const client)
 {
-    t_replyBatch batch;
-    const std::vector<std::string>& params = msg.getParams();
+	t_replyBatch batch;
+	const std::vector<std::string>& params = msg.getParams();
 
-    if (params.size() < 2)
-    {
-        batch = obj->errNeedMoreParams(client, msg);
-        return batch;
-    }
+	if (params.size() < 2)
+	{
+		batch = obj->errNeedMoreParams(client, msg);
+		return batch;
+	}
 
-    const std::string& target = params[0];
-    const std::string& message = params[1];
+	const std::string&	message = params[1];
 
-    if (message.empty())
-    {
-        return batch;
-    }
+	if (message.empty())
+	{
+		return batch;
+	}
+	
+	std::vector<std::string>	target_vec = ft_std::split(params.at(0), ",");
 
-    Message reply;
+	for (std::vector<std::string>::iterator it = target_vec.begin(); it != target_vec.end(); it++)
+	{
+		const std::string& target = *it;
 
-    reply.setSource(client->constructSource());
-    reply.setCommand("PRIVMSG");
-    reply.pushParam(target);
-    reply.pushParam(message);
+		Message reply;
 
-    if (target[0] == '#')
-    {
-        Channel* channel = obj->getChannelByName(target);
+		reply.setSource(client->constructSource());
+		reply.setCommand("PRIVMSG");
+		reply.pushParam(target);
+		reply.pushParam(message);
 
-        if (!channel)
-        {
-            Message err;
-            err.setSource(obj->getServerName());
-            err.setCommand(ERR_NOSUCHCHANNEL);
-            err.pushParam(client->getNickname());
-            err.pushParam(target);
-            err.pushParam("No such channel");
+		if (target[0] == '#')
+		{
+			Channel* channel = obj->getChannelByName(target);
 
-            t_reply reply_item;
-            reply_item.first = client;
-            reply_item.second.push(err);
-            batch.push_back(reply_item);
+			if (!channel)
+			{
+				Message err;
+				err.setSource(obj->getServerName());
+				err.setCommand(ERR_NOSUCHCHANNEL);
+				err.pushParam(client->getNickname());
+				err.pushParam(target);
+				err.pushParam("No such channel");
 
-            return batch;
-        }
+				t_reply reply_item;
+				reply_item.first = client;
+				reply_item.second.push(err);
+				batch.push_back(reply_item);
 
-        if (!channel->hasThisClient(client))
-        {
-            Message err;
-            err.setSource(obj->getServerName());
-            err.setCommand(ERR_CANNOTSENDTOCHAN);
-            err.pushParam(client->getNickname());
-            err.pushParam(target);
-            err.pushParam("Cannot send to channel");
+				continue ;
+			}
 
-            t_reply reply_item;
-            reply_item.first = client;
-            reply_item.second.push(err);
-            batch.push_back(reply_item);
+			if (!channel->hasThisClient(client))
+			{
+				Message err;
+				err.setSource(obj->getServerName());
+				err.setCommand(ERR_CANNOTSENDTOCHAN);
+				err.pushParam(client->getNickname());
+				err.pushParam(target);
+				err.pushParam("Cannot send to channel");
 
-            return batch;
-        }
-        obj->pushChannelReplyOthers(reply, channel, batch, client);
-    }
-    else
-    {
-        Client* target_client = obj->getClientByNickname(target);
+				t_reply reply_item;
+				reply_item.first = client;
+				reply_item.second.push(err);
+				batch.push_back(reply_item);
 
-        if (!target_client)
-        {
-            Message err;
-            err.setSource(obj->getServerName());
-            err.setCommand(ERR_NOSUCHNICK);
-            err.pushParam(client->getNickname());
-            err.pushParam(target);
-            err.pushParam("No such nick/channel");
+				continue;
+			}
+			
+			obj->pushChannelReplyOthers(reply, channel, batch, client);
+			continue ;
 
-            t_reply reply_item;
-            reply_item.first = client;
-            reply_item.second.push(err);
-            batch.push_back(reply_item);
+		}
+		else
+		{
+			Client* target_client = obj->getClientByNickname(target);
 
-            return batch;
-        }
+			if (!target_client)
+			{
+				Message err;
+				err.setSource(obj->getServerName());
+				err.setCommand(ERR_NOSUCHNICK);
+				err.pushParam(client->getNickname());
+				err.pushParam(target);
+				err.pushParam("No such nick/channel");
 
-        t_reply reply_item;
-        reply_item.first = target_client;
-        reply_item.second.push(reply);
-        batch.push_back(reply_item);
-    }
+				t_reply reply_item;
+				reply_item.first = client;
+				reply_item.second.push(err);
+				batch.push_back(reply_item);
 
-    return batch;
+				continue ;
+			}
+
+			t_reply reply_item;
+			reply_item.first = target_client;
+			reply_item.second.push(reply);
+			batch.push_back(reply_item);
+
+			continue ;
+		}
+	}
+
+	return batch;
 }
