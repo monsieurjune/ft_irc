c296cb82c109d1134f5dccb7278956efbd36ba71
monsieurjune <june.monsieurjune@gmail.com>
2025-05-18 07:11:07 +0700

    (refactor): move logger's macros to header file

--- include/utils/ft_utils.hpp
+++ include/utils/ft_utils.hpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:01:25 by tponutha          #+#    #+#             */
-/*   Updated: 2025/05/17 02:05:01 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/05/18 07:01:57 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,47 @@
 
 // CPP Header
 #include <string>
+
+// ANSI color codes
+#ifndef COLOR_RESET
+#define COLOR_RESET     "\033[0m"
+#endif
+
+#ifndef COLOR_BOLD
+#define COLOR_BOLD      "\033[1m"
+#endif
+
+#ifndef COLOR_BLACK
+#define COLOR_BLACK     "\033[30m"
+#endif
+
+#ifndef COLOR_RED
+#define COLOR_RED       "\033[31m"
+#endif
+
+#ifndef COLOR_GREEN
+#define COLOR_GREEN     "\033[32m"
+#endif
+
+#ifndef COLOR_YELLOW
+#define COLOR_YELLOW    "\033[33m"
+#endif
+
+#ifndef COLOR_BLUE
+#define COLOR_BLUE      "\033[34m"
+#endif
+
+#ifndef COLOR_MAGENTA
+#define COLOR_MAGENTA   "\033[35m"
+#endif
+
+#ifndef COLOR_CYAN
+#define COLOR_CYAN      "\033[36m"
+#endif
+
+#ifndef COLOR_WHITE
+#define COLOR_WHITE     "\033[37m"
+#endif
 
 namespace ft_utils
 {
--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -6,7 +6,7 @@
 /*   By: tponutha <tponutha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 20:13:51 by tponutha          #+#    #+#             */
-/*   Updated: 2025/05/17 02:44:28 by tponutha         ###   ########.fr       */
+/*   Updated: 2025/05/18 07:00:38 by tponutha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,6 @@
 #include <ctime>
 #include <string>
 #include <iostream>
-
-// ANSI color codes
-#define COLOR_RESET     "\033[0m"
-#define COLOR_BOLD      "\033[1m"
-
-#define COLOR_BLACK     "\033[30m"
-#define COLOR_RED       "\033[31m"
-#define COLOR_GREEN     "\033[32m"
-#define COLOR_YELLOW    "\033[33m"
-#define COLOR_BLUE      "\033[34m"
-#define COLOR_MAGENTA   "\033[35m"
-#define COLOR_CYAN      "\033[36m"
-#define COLOR_WHITE     "\033[37m"
 
 namespace ft_utils
 {
